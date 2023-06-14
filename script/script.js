9d47e7b9f0b98a6516499e5e40cb93adeb2ff81c
Gustavo Toná <gustavopicolitona@gmail.com>
2023-06-14 13:10:53 -0300

    update 1.1 / 14.06

--- script/script.js
+++ script/script.js
@@ -1,45 +1,12 @@
-const btnTorneira = document.getElementById("btn-torneira");
-const contadorTorneira = document.getElementById("contador-torneira");
+const btnEnergy = document.getElementById("btn-energy");
+const counterFinal = document.getElementById("counter-final");
 
 let contadorT = 0;
 
-contadorTorneira.innerHTML = contadorT;
+counterFinal.innerHTML = contadorT;
 
-btnTorneira.addEventListener("click", function(){
-   contadorTorneira.innerHTML = ++contadorT;
+btnEnergy.addEventListener("click", function(){
+   counterFinal.innerHTML = ++contadorT;
 });
 
 console.log(contadorT);
-
-
-// Contador vazio 
-
-const bntVazio = document.getElementById("btn-vazio");
-const contadorVazio = document.getElementById("contador-vazio")
-
-let contadorV = 0;
-
-contadorVazio.innerHTML = contadorV
-
-bntVazio.addEventListener("click", function(){
-    contadorVazio.innerHTML = ++contadorV;
-});
-
-
-// Contador final | Deve somar os valores e somar 1 
-
-const contadorFinal = document.getElementById("contador-final");
-
-btnTorneira.addEventListener("click", function(){
-
-    
-    contadorFinal.innerHTML = contadorV + contadorT;
-});
-
-
-bntVazio.addEventListener("click", function(){
-
-    if(contadorT >= 1)
-    contadorFinal.innerHTML = contadorV + contadorT;
-});
-
